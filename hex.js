8e1f6bb215db4963e6e50d25f2596205ef62a676
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-01-12 02:01:50 +0200

    added the hex color when you click the button

--- hex.js
+++ hex.js
@@ -1,1 +1,18 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+	let hexColor = '#';
+	for (let index = 0; index < 6; index++) {
+		const randomNumber = getRnadomNumber();
+		hexColor += hex[randomNumber];
+	}
+	document.body.style.backgroundColor = hexColor;
+	color.textContent = hexColor;
+});
+
+function getRnadomNumber() {
+	return Math.floor(Math.random() * hex.length);
+}
